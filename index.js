9471d95c43d06de609f2721cff35c13615e42b2a
Ansar <moha.ans@hotmail.com>
2022-02-02 22:22:23 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const rp = require('request-promise');
+const $ = require('cheerio').default;
+const { htmlToText } = require('html-to-text');
+
+function IndeedKey(link){
+    const words = link.split('?');
+    console.log(words);
+    const words2 = words[1].split('&');
+    console.log(words2);
+    var jk ='';
+    for(var i = 0; i< words2.length;i++){
+
+      if(words2[i].match('^jk')){
+        const words3 = words2[i].split('=');
+        return jk = words3[1];
+      }
+    }
+
+}
+
+function IndeedData(link){
+    const linkJobDetails = 'https://fr.indeed.com/voir-emploi?viewtype=embedded&jk='+link;
+    rp(linkJobDetails)
+    .then(function(html){
+        var postTitle = $("h1",html).text();
+        var typeContract,companyName,lieu,description;
+        companyName = $("#viewJobSSRRoot > div > div.icl-Container--fluid.fs-unmask.jobsearch-ViewJobLayout-fluidContainer.is-FR > div > div > div > div.jobsearch-ViewJobLayout-jobDisplay.icl-Grid-col.icl-u-xs-span12.icl-u-lg-span7 > div > div.jobsearch-JobComponent-embeddedHeader > div > div:nth-child(2) > div.jobsearch-CompanyInfoContainer > div > div > div > div:nth-child(2) > div",html).html();
+        if(companyName == null){
+          companyName = $("#viewJobSSRRoot > div > div.icl-Container--fluid.fs-unmask.jobsearch-ViewJobLayout-fluidContainer.is-FR > div > div > div > div.jobsearch-ViewJobLayout-jobDisplay.icl-Grid-col.icl-u-xs-span12.icl-u-lg-span7 > div > div.jobsearch-JobComponent-embeddedHeader > div > div:nth-child(1) > div.jobsearch-CompanyInfoContainer > div > div > div > div.jobsearch-InlineCompanyRating.icl-u-xs-mt--xs.icl-u-xs-mb--md > div:nth-child(2) > div",html).text();
+          typeContract = $("#viewJobSSRRoot > div > div.icl-Container--fluid.fs-unmask.jobsearch-ViewJobLayout-fluidContainer.is-FR > div > div > div > div.jobsearch-ViewJobLayout-jobDisplay.icl-Grid-col.icl-u-xs-span12.icl-u-lg-span7 > div > div.jobsearch-JobComponent-embeddedHeader > div > div:nth-child(2) > div:nth-child(1) > div",html).text();
+          lieu = $("#viewJobSSRRoot > div > div.icl-Container--fluid.fs-unmask.jobsearch-ViewJobLayout-fluidContainer.is-FR > div > div > div > div.jobsearch-ViewJobLayout-jobDisplay.icl-Grid-col.icl-u-xs-span12.icl-u-lg-span7 > div > div.jobsearch-JobComponent-embeddedHeader > div > div:nth-child(1) > div.jobsearch-CompanyInfoContainer > div > div > div > div:nth-child(2) > div",html).html();
+          
+        }else{
+          typeContract = $("#viewJobSSRRoot > div > div.icl-Container--fluid.fs-unmask.jobsearch-ViewJobLayout-fluidContainer.is-FR > div > div > div > div.jobsearch-ViewJobLayout-jobDisplay.icl-Grid-col.icl-u-xs-span12.icl-u-lg-span7 > div > div.jobsearch-JobComponent-embeddedHeader > div > div:nth-child(3) > div:nth-child(1) > div > span",html).html();
+          companyName = $("#viewJobSSRRoot > div > div.icl-Container--fluid.fs-unmask.jobsearch-ViewJobLayout-fluidContainer.is-FR > div > div > div > div.jobsearch-ViewJobLayout-jobDisplay.icl-Grid-col.icl-u-xs-span12.icl-u-lg-span7 > div > div.jobsearch-JobComponent-embeddedHeader > div > div:nth-child(2) > div.jobsearch-CompanyInfoContainer > div > div > div > div.jobsearch-InlineCompanyRating.icl-u-xs-mt--xs.icl-u-xs-mb--md > div:nth-child(2) > div > a",html).html();
+      
+        }
+        const listItems = $(".jobsearch-jobDescriptionText",html);
+        listItems.each(function (idx, el) {
+          description += $(el).html();
+        });
+        description = htmlToText(description);
+
+        return { userId: '1', jobTitle :postTitle, location : lieu, nomEntreprise : companyName,typeDeContrat : typeContract, descriptionPost :description };
+
+    })
+    .catch(function(err){
+        console.log(err);
+    });
+}
+
+
+
+module.exports.IndeedData = IndeedData;
+module.exports.IndeedKey = IndeedKey;
+
